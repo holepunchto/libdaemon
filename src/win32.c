cf90566a5039421da8e1048ab88e52a036815d7c
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-04-02 16:18:55 +0200

    Handle `argv` being `NULL` on Windows

--- src/win32.c
+++ src/win32.c
@@ -1,14 +1,21 @@
 #include <process.h>
+#include <windows.h>
 
 #include "../include/daemon.h"
 
 int
 daemon_spawn(daemon_t *daemon, const char *file, const char *const argv[], const char *const env[]) {
+  const char *default_argv[2] = {file, NULL};
+
+  if (argv == NULL) argv = default_argv;
+
   intptr_t handle = _spawnve(_P_NOWAIT, file, argv, env);
 
   if (handle == -1) return -1;
 
-  daemon->pid = (int) handle;
+  daemon->pid = GetProcessId((HANDLE) handle);
+
+  CloseHandle((HANDLE) handle);
 
   return 0;
 }
