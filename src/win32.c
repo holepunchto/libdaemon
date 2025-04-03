71a407697186201f6fdeb33824ac59a388b6dc6b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-04-03 09:22:18 +0200

    Properly detach process on Windows

--- src/win32.c
+++ src/win32.c
@@ -1,21 +1,39 @@
-#include <process.h>
+#include <stdio.h>
 #include <windows.h>
 
 #include "../include/daemon.h"
 
 int
 daemon_spawn(daemon_t *daemon, const char *file, const char *const argv[], const char *const env[]) {
-  const char *default_argv[2] = {file, NULL};
+  STARTUPINFOA si;
+  ZeroMemory(&si, sizeof(si));
 
-  if (argv == NULL) argv = default_argv;
+  si.cb = sizeof(si);
 
-  intptr_t handle = _spawnve(_P_NOWAIT, file, argv, env);
+  PROCESS_INFORMATION pi;
+  ZeroMemory(&pi, sizeof(pi));
 
-  if (handle == -1) return -1;
+  char cmd[MAX_PATH];
+  snprintf(cmd, sizeof(cmd), "\"%s\"", file);
 
-  daemon->pid = GetProcessId((HANDLE) handle);
+  BOOL success = CreateProcessA(
+    NULL,
+    cmd,
+    NULL,
+    NULL,
+    FALSE,
+    CREATE_NO_WINDOW | DETACHED_PROCESS,
+    NULL,
+    NULL,
+    &si,
+    &pi
+  );
 
-  CloseHandle((HANDLE) handle);
+  if (!success) return -1;
+
+  CloseHandle(pi.hThread);
+
+  daemon->pid = (int) pi.dwProcessId;
 
   return 0;
 }
